61a0c09ac1d6f0a3960953058933e7a4affdb474
Bob Silverberg <bob.silverberg@gmail.com>
2015-03-31 16:07:34 -0400

    updates

--- marketplacetests/in_app_payments/in_app.py
+++ marketplacetests/in_app_payments/in_app.py
@@ -2,15 +2,19 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette import expected, By, Wait
 from gaiatest.apps.base import Base
+from gaiatest.apps.base import PageRegion
+from marionette import expected, By, Wait
+
+from marketplacetests.firefox_accounts.app import FirefoxAccounts
 
 
 class InAppPayment(Base):
 
     # Products
+    _available_products_locator = (By.ID, 'items')
+    _available_product_locator = (By.CSS_SELECTOR, '#items li')
     _bought_product_locator = (By.CSS_SELECTOR, '#bought .item > h4')
-    _buy_product_button_locator = (By.XPATH, "//h4[text()='%s']/following-sibling::button")
     _server_select_locator = (By.ID, 'server')
 
     def __init__(self, marionette, server):
@@ -28,13 +32,34 @@ class InAppPayment(Base):
         self.select(server)
         self.apps.switch_to_displayed_app()
 
-    def tap_buy_product(self, text):
-        element = Wait(self.marionette).until(
-            expected.element_present(self._buy_product_button_locator[0], self._buy_product_button_locator[1] % text))
-        Wait(self.marionette).until(expected.element_displayed(element))
-        element.tap()
-        from marketplacetests.firefox_accounts.app import FirefoxAccounts
-        return FirefoxAccounts(self.marionette)
+    def tap_buy_product(self, name):
+        for product in self.available_products:
+            if product.name == name:
+                product.tap_buy_button()
+                return FirefoxAccounts(self.marionette)
+        raise Exception('Unable to find and tap on product %s.'
+                        % name)
 
     def wait_for_bought_products_displayed(self):
         self.wait_for_element_displayed(*self._bought_product_locator)
+
+    @property
+    def available_products(self):
+        element = Wait(self.marionette).until(
+            expected.element_present(*self._available_products_locator))
+        Wait(self.marionette).until(expected.element_displayed(element))
+        products = self.marionette.find_elements(*self._available_product_locator)
+        return [Product(self.marionette, product) for product in products]
+
+
+class Product(PageRegion):
+
+    _buy_button_locator = (By.CSS_SELECTOR, 'button')
+    _name_locator = (By.CSS_SELECTOR, 'h4')
+
+    @property
+    def name(self):
+        return self.root_element.find_element(*self._name_locator).text
+
+    def tap_buy_button(self):
+        self.root_element.find_element(*self._buy_button_locator).tap()
